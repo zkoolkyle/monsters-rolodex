77f872d4cc876219f5dbead0af1f849de27dcbee
Kyle <zkoolkyle@gmail.com>
2020-07-27 16:05:23 -0400

    Updates

---
+++ precache-manifest.37b3d884f4306de776aaabff19c3fc27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396b17fd01fb4eb7c9f44d92c26ea06c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7dbbebb18b7bd3c50013",
+    "url": "/monsters-rolodex/static/css/main.46281eec.chunk.css"
+  },
+  {
+    "revision": "e4408aa8b874d405651c",
+    "url": "/monsters-rolodex/static/js/2.d7f8b0b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d7f8b0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbbebb18b7bd3c50013",
+    "url": "/monsters-rolodex/static/js/main.dc41a859.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
